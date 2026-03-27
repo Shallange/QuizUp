e39ef70fc6c06147c5f536dc67cfa2df01393eef
Shallange <jimmy.kroneld@hotmail.com>
2026-03-27 14:12:01 +0100

    Initial Commit

---
+++ QuizUp/QuizUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizUpApp.swift
+//  QuizUp
+//
+//  Created by Jimmy kroneld on 2026-03-27.
+//
+
+import SwiftUI
+
+@main
+struct QuizUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
