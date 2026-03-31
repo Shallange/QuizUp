8d25fa238e6b8a839f5b202f353d4451d532dde2
Shallange <jimmy.kroneld@hotmail.com>
2026-03-31 19:41:02 +0200

    refactored project structure and renamed QuizTesting to QuizView

---
+++ QuizUp/Data/QuizData.swift
@@ -0,0 +1,14 @@
+//
+//  QuizData.swift
+//  QuizUp
+import Foundation
+
+struct QuizData{
+    
+    static let questionList = [
+        Question(text: "What shape is Earth1", choices: ["Flat", "Ball", "Can"], correctAnswer: 0),
+        Question(text: "What shape is Earth2", choices: ["Flat", "Ball", "Can"], correctAnswer: 2),
+        Question(text: "What shape is Earth3", choices: ["Flat", "Ball", "Can"], correctAnswer: 1),
+        Question(text: "What shape is Earth4", choices: ["Flat", "Ball", "Can"], correctAnswer: 1)
+    ]
+}
