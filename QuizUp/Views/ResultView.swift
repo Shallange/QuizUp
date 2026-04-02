41b4feeebca5dd1bc602a5d187ec715e9e80f7ee
GreedyGobling <greedygobling@gmail.com>
2026-04-02 17:31:12 +0200

    made ui nicer

--- QuizUp/Views/ResultView.swift
+++ QuizUp/Views/ResultView.swift
@@ -29,6 +29,10 @@ struct ResultView: View {
                         Text("Your answer: \(answers[i])")
                             .foregroundColor(wasCorrect ? .green : .red)
                     }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
                 } // eac
             } // end Vstack
             .padding()
